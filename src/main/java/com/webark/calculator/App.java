f113b70e7ec0c6cb9e5110e8fbf0cbf043c36f0f
Chathuranga <chathu.hasantha@gmail.com>
2025-10-28 15:39:31 +0530

    Java Calculator - Initial Project

---
+++ src/main/java/com/webark/calculator/App.java
@@ -0,0 +1,10 @@
+package com.webark.calculator;
+
+import com.webark.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
